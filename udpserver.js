427c18403ddf2de8d0df47f438ab1bb2ee3ea8af
Sam Hiatt <sam.hiatt@gmail.com>
2015-05-19 13:27:33 -0700

    Added UDP server for testing.

---
+++ udpserver.js
@@ -0,0 +1,23 @@
+// Reference implementation from https://nodejs.org/api/dgram.html
+
+var dgram = require("dgram");
+
+var server = dgram.createSocket("udp4");
+
+server.on("error", function (err) {
+	console.log("server error:\n" + err.stack);
+	server.close();
+});
+
+server.on("message", function (msg, rinfo) {
+	console.log("server got: " + msg + " from " +
+	rinfo.address + ":" + rinfo.port);
+});
+
+server.on("listening", function () {
+	var address = server.address();
+	console.log("server listening " +
+	address.address + ":" + address.port);
+});
+
+server.bind(8899);
